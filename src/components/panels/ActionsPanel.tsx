7e62fd9a9c0521d009161211135cd5e2a3559b8c
Kyrylo Khomenko <kkhomenko10@gmail.com>
2024-12-13 15:46:19 +0100

    Delete undo/redo icons

--- src/components/panels/ActionsPanel.tsx
+++ src/components/panels/ActionsPanel.tsx
@@ -1,20 +1,24 @@
 import { APP_ICON_SIZE } from '../../constants';
-import { useDrawingContext } from '../../hooks';
+// import { useDrawingContext } from '../../hooks';
 import { downloadCanvas } from '../../utils';
 import Button from '../Button';
-import { Undo, Redo, Download } from 'lucide-react';
+import {
+  // Undo,
+  // Redo,
+  Download,
+} from 'lucide-react';
 
 const ActionsPanel = () => {
-  const { undo, redo } = useDrawingContext();
+  // const { undo, redo } = useDrawingContext();
 
   return (
     <div className="widget absolute left-4 bottom-4 !flex-row gap-4">
-      <Button tooltip="Undo Action" onClick={undo}>
+      {/* <Button tooltip="Undo Action" onClick={undo}>
         <Undo size={APP_ICON_SIZE} />
       </Button>
       <Button tooltip="Redo Action" onClick={redo}>
         <Redo size={APP_ICON_SIZE} />
-      </Button>
+      </Button> */}
       <Button
         tooltip="Download Drawing"
         onClick={() => downloadCanvas('drawing.png')}
